1d4cfd2b233ad397a6ba1a383c3d6dae6d57e89a
지민성 <jiminsung@naver.com>
2023-10-22 03:32:51 +0900

    rename

--- src/lecture/3DCanvas.jsx
+++ src/lecture/3DCanvas.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import MyElement3D from "./5. Geomerty/MyElement3D5_5_torus";
+import MyElement3D from "./5. Geomerty/Geomerty_5_torus";
 
 const ThreeDemensionCanvas = () => {
   return(
