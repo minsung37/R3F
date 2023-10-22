d171b797c91033dbc89286991ba394ec9425601b
지민성 <jiminsung@naver.com>
2023-10-22 17:03:59 +0900

    Camera

--- src/lecture/3DCanvas.jsx
+++ src/lecture/3DCanvas.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import MyElement3D from "./7. Light/Light_7_Enviroment"
+import MyElement3D from "./8. Camera/Camera_3_Orthography"
 
 
 const ThreeDemensionCanvas = () => {
@@ -7,8 +7,26 @@ const ThreeDemensionCanvas = () => {
     <>
       <Canvas 
         // camera={{ near: 3.5, far: 6 }}
-        // Light에 사용
-        camera={{ fov: 75, position:[7, 7, 0] }}
+        
+        // Light
+        // camera={{ fov: 75, position:[7, 7, 0] }}
+        
+        // perspective camera
+        // camera={{ 
+        //   fov: 130,
+        //   near: 0.1,
+        //   // far: 10,
+        //   position:[7, 7, 0]
+        // }}
+
+        // orthography camera
+        orthographic
+        camera={{
+          near: 0.1,
+          far: 20,
+          zoom: 100,
+          position:[7, 7, 0]
+        }}
       >
         <MyElement3D/>
       </Canvas>
---
+++ src/lecture/8. Camera/Camera_1_Perspective.jsx
@@ -0,0 +1,101 @@
+import { OrbitControls, useHelper } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { RectAreaLightUniformsLib } from "three-stdlib";
+import { RectAreaLightHelper } from "three-stdlib";
+
+const torusGeometry = new THREE.TorusGeometry(0.4, 0.1, 32, 32)
+const torusMaterial = new THREE.MeshStandardMaterial({
+  color: "#9b59b6",
+  roughness: 0.5,
+  metalness: 0.9
+})
+
+RectAreaLightUniformsLib.init();
+
+const MyElement3D = () => {
+
+  useFrame((state) => {
+    const time = state.clock.elapsedTime
+    const smallSpherePivot = state.scene.getObjectByName("smallSpherePivot");
+    smallSpherePivot.rotation.y = THREE.MathUtils.degToRad(time * 50);
+
+  })
+
+  const light = useRef();
+  useHelper(light, RectAreaLightHelper);
+
+  const { camera } = useThree();
+
+  useControls({
+    positionZ: {
+      value: 0, min: -10, max: 10, step: 0.1,
+      onChange: (v) => camera.position.z = v
+    },
+    targetZ: {
+      value: 0, min: -10, max: 10, step: 0.1,
+      onChange: (v) => camera.lookAt(0, 0, v)
+    }
+  })
+
+  return(
+    <>
+      <OrbitControls />
+      <rectAreaLight
+        ref={light}
+        color={0xffffff}
+        intensity={20}
+        width={2}
+        height={5}
+        position={[0, 5, 0]}
+        rotation-x={THREE.MathUtils.degToRad(-90)}
+      />
+      <mesh rotation-x={THREE.MathUtils.degToRad(-90)}>
+        <planeGeometry args={[10, 10]} />
+        <meshStandardMaterial 
+          color="#2c3e50"
+          roughness={0.5}
+          metalness={0.5}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+
+      <mesh rotation-x={THREE.MathUtils.degToRad(-90)}>
+        <sphereGeometry args={[1.5, 64, 64, 0, Math.PI]} />
+        <meshStandardMaterial 
+          color="#ffffff"
+          roughness={0.1}
+          metalness={0.2}
+        />
+      </mesh>
+
+      {new Array(8).fill().map((item, index) => {
+        return (
+          <group key={index} rotation-y={THREE.MathUtils.degToRad(45 * index)}>
+            <mesh
+              geometry={torusGeometry}
+              material={torusMaterial}
+              position={[3, 0.5, 0]}
+            />
+          </group>
+        )
+      })}
+
+      <group name="smallSpherePivot">
+        <mesh position={[3, 0.5, 0]}>
+          <sphereGeometry args={[0.3, 32, 32]} />
+          <meshStandardMaterial
+            color="#e74c3c"
+            roughness={0.2}
+            metalness={0.5}
+          />
+        </mesh>
+      </group>
+    </>
+  )
+}
+
+
+export default MyElement3D;
